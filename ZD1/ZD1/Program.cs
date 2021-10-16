b53c804239c0721ace82597eede0cd4bddbb25a0
k1dobu3 <91422323+k1dobu3@users.noreply.github.com>
2021-10-16 10:30:46 +0300

    Add files via upload

---
+++ ZD1/ZD1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ZD1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            if (1<=a && a<=3) Console.WriteLine(a);
+            else Console.WriteLine();
+
+            if (1<=b && b<=3) Console.WriteLine(b);
+            else Console.WriteLine();
+
+            if (1<=c && c<=3) Console.WriteLine(c);
+            else Console.WriteLine();
+
+
+        }
+    }
+}
